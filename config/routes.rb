250d460fd53f9138b07684e4ee2abcc028600993
See Bei <seelowbei@gmail.com>
2015-06-10 12:00:22 +0800

    Restricted route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Crmfieldupdate::Application.routes.draw do
 
   devise_for :users
-  resources :mappings
+  resources :mappings, :only => [:index, :new, :create]
   root to: "mappings#index"
   match "/passwords/edit", via: :get
   match "/passwords", via: :put, action: :update, controller: :passwords
